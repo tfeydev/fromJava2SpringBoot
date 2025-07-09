c2c56e29eaeb55838ae37fdd8059ac463a6b6f38
tfeydev <tfeydeveloper@gmail.com>
2025-07-08 22:03:26 -0300

    updating

---
+++ springboot/01-SpringBoot-Quickstart/03-actuator-demo/src/test/java/br/com/techthor/springboot/demo/mycoolapp/MycoolappApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.techthor.springboot.demo.mycoolapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MycoolappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
