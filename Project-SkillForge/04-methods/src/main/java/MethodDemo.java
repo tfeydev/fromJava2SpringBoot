60b865cbe6df195bd49bc1b0e38d78b9bc07ec6c
tfeydev <tfeydeveloper@gmail.com>
2025-07-09 19:50:30 -0300

    update methods

---
+++ Project-SkillForge/04-methods/src/main/java/MethodDemo.java
@@ -0,0 +1,17 @@
+public class MethodDemo {
+
+    public static void main(String[] args) {
+
+        // call the method
+        displayGreetings();
+
+    }
+
+    // define the method
+    public static void displayGreetings() {
+        System.out.println("Hello world!");
+        System.out.println("Welcome, welcome.");
+        System.out.println("Please make yourself at home.");
+    }
+
+}
