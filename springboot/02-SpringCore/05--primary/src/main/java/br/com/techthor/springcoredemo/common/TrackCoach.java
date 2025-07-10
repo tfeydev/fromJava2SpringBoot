8502f66548df91a1bc39ae2d60a116f5b2a76e96
tfeydev <tfeydeveloper@gmail.com>
2025-07-09 21:18:53 -0300

    update primary

---
+++ springboot/02-SpringCore/05--primary/src/main/java/br/com/techthor/springcoredemo/common/TrackCoach.java
@@ -0,0 +1,15 @@
+package br.com.techthor.springcoredemo.common;
+
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+@Component
+@Primary
+public class TrackCoach implements Coach{
+
+    @Override
+    public String getDailyWorkout() {
+        return "Run a hard 5k!";
+
+    }
+}
