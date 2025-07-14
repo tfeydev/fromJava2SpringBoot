8312448c07d64d40a5f89cd4a0927d1d1b629c2f
tfeydev <tfeydeveloper@gmail.com>
2025-07-14 16:39:21 -0300

    adding taco-cloud

---
+++ java/05-arrays/src/main/java/ArrayDemo.java
@@ -0,0 +1,22 @@
+public class ArrayDemo {
+
+    public static void main(String[] args) {
+
+        // initialize the array
+        String[] colors = { "Red", "Green", "Blue", "Yellow"};
+
+        // display contnts of the array
+        System.out.println("Contents of the array:");
+        System.out.println(colors[0]);
+        System.out.println(colors[1]);
+        System.out.println(colors[2]);
+        System.out.println(colors[3]);
+
+        System.out.println();
+
+        // display length of the array
+        System.out.println("Length of the array: " + colors.length);
+
+        System.out.println();
+    }
+}
---
+++ tacocloud-learn/tacocloud/src/test/java/br/com/techthor/tacocloud/TacocloudApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.techthor.tacocloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TacocloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
