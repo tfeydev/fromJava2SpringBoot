228ba60e287e986c6c643daad74dca431ab458de
tfeydev <tfeydeveloper@gmail.com>
2025-07-21 23:12:02 -0300

    update angular training

--- Spring-Boot-Angular-Fullstack/angular-training/sales-project/src/app/app.ts
+++ Spring-Boot-Angular-Fullstack/angular-training/sales-project/src/app/app.ts
@@ -1,10 +1,9 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { SalesPersonList } from "./sales-person-list/sales-person-list";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, SalesPersonList],
+  imports: [SalesPersonList],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
