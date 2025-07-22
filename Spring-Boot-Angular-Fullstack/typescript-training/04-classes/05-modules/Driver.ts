beb187353e9daa3880ed1dc5a87bfe0b77e2d0a5
tfeydev <tfeydeveloper@gmail.com>
2025-07-21 21:51:57 -0300

    update typescript-training

---
+++ Spring-Boot-Angular-Fullstack/typescript-training/04-classes/05-modules/Driver.ts
@@ -0,0 +1,7 @@
+import { Customer } from "./Customer";
+
+// lett's create an instance of the Customer class
+let myCustomer = new Customer("Martin", "Dixon");
+
+console.log(myCustomer.firstName);
+console.log(myCustomer.lastName);
