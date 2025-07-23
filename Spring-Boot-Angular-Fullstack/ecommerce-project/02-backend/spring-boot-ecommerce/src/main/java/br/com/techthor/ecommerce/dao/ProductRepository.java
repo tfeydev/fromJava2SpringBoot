4dcf98a8138276e3a8dc2346c10d2f157752dbc0
tfeydev <tfeydeveloper@gmail.com>
2025-07-22 22:27:35 -0300

    created and updated -- backend

--- Spring-Boot-Angular-Fullstack/ecommerce-project/02-backend/spring-boot-ecommerce/src/main/java/br/com/techthor/ecommerce/dao/ProductRepository.java
+++ Spring-Boot-Angular-Fullstack/ecommerce-project/02-backend/spring-boot-ecommerce/src/main/java/br/com/techthor/ecommerce/dao/ProductRepository.java
@@ -4,7 +4,9 @@ import br.com.techthor.ecommerce.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.rest.core.annotation.RestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin("http://localhost:4200")
 @RepositoryRestResource
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
