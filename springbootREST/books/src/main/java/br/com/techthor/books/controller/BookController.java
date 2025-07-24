18c86675bd18e1cab9204b0820e2ec3d02b177c6
tfeydev <tfeydeveloper@gmail.com>
2025-07-24 09:17:35 -0300

    update Spring Boot REST - Project 1 "Books"

--- springbootREST/books/src/main/java/br/com/techthor/books/controller/BookController.java
+++ springbootREST/books/src/main/java/br/com/techthor/books/controller/BookController.java
@@ -1,14 +1,35 @@
 package br.com.techthor.books.controller;
 
+import br.com.techthor.books.entity.Book;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class BookController {
 
-    @GetMapping("/api")
-    public String firstAPI() {
-        return "Hello Thor!";
+    private final List<Book> books = new ArrayList<>();
+
+    public BookController() {
+        initializeBooks();
+    }
+
+    private void initializeBooks() {
+        books.addAll(List.of(
+                new Book("Title one", "Author one", "science"),
+                new Book("Title two", "Author two", "science"),
+                new Book("Title three", "Author three", "history"),
+                new Book("Title four", "Author four", "math"),
+                new Book("Title five", "Author five", "math"),
+                new Book("Title six", "Author six", "math")
+        ));
+    }
+
+    @GetMapping("/api/books")
+    public List<Book> getBooks() {
+        return books;
     }
 
 }
