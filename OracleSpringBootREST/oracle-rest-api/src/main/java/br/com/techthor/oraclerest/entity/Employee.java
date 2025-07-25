52655d9f7077ca5697fa40b2e84417291774871f
tfeydev <tfeydeveloper@gmail.com>
2025-07-25 00:37:28 -0300

    updated

--- OracleSpringBootREST/oracle-rest-api/src/main/java/br/com/techthor/oraclerest/entity/Employee.java
+++ OracleSpringBootREST/oracle-rest-api/src/main/java/br/com/techthor/oraclerest/entity/Employee.java
@@ -2,6 +2,8 @@ package br.com.techthor.oraclerest.entity;
 
 import jakarta.persistence.*;
 
+import java.sql.Date;
+
 @Entity
 @Table(name = "EMPLOYEES")
 public class Employee {
@@ -30,4 +32,59 @@ public class Employee {
 
     // getters/setters
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public Date getHireDate() {
+        return hireDate;
+    }
+
+    public void setHireDate(Date hireDate) {
+        this.hireDate = hireDate;
+    }
+
+    public String getJobId() {
+        return jobId;
+    }
+
+    public void setJobId(String jobId) {
+        this.jobId = jobId;
+    }
 }
