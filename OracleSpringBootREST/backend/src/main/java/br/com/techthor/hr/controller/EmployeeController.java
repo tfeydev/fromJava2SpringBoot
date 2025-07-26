78201a6c913aa68dd0eabbeaf1762ce7fe6e85eb
tfeydev <tfeydeveloper@gmail.com>
2025-07-25 21:13:07 -0300

    update backend

--- OracleSpringBootREST/backend/src/main/java/br/com/techthor/hr/controller/EmployeeController.java
+++ OracleSpringBootREST/backend/src/main/java/br/com/techthor/hr/controller/EmployeeController.java
@@ -2,12 +2,11 @@ package br.com.techthor.hr.controller;
 
 import br.com.techthor.hr.entity.Employee;
 import br.com.techthor.hr.repository.EmployeeRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
@@ -24,8 +23,8 @@ public class EmployeeController {
     }
 
     @GetMapping
-    public List<Employee> getAll() {
-        return repository.findAll();
+    public Page<Employee> getAll(@RequestParam int page, @RequestParam int size) {
+        return repository.findAll(PageRequest.of(page, size));
     }
 
     @GetMapping("/{id}")
