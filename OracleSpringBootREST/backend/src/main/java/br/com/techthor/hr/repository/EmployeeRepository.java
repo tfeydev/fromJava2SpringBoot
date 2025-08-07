d43ad46f472943e10f950acc88a0062d8cdb8de5
tfeydev <tfeydeveloper@gmail.com>
2025-08-07 20:47:33 -0300

    delete unused

--- OracleSpringBootREST/backend/src/main/java/br/com/techthor/hr/repository/EmployeeRepository.java
+++
@@ -1,8 +0,0 @@
-package br.com.techthor.hr.repository;
-
-import br.com.techthor.hr.entity.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-
-}
