48a2fba7ad43f4d3355009a0d020710f3e336475
tfeydev <tfeydeveloper@gmail.com>
2025-08-19 21:07:05 -0300

    update corsorigin

--- SpringBootAngularOracleXE/backend/src/main/java/br/com/techthor/ecommerce/config/CorsConfig.java
+++ SpringBootAngularOracleXE/backend/src/main/java/br/com/techthor/ecommerce/config/CorsConfig.java
@@ -1,6 +1,5 @@
 package br.com.techthor.ecommerce.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -9,18 +8,15 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig {
 
-    @Value("${FRONTEND_URL}")
-    private String frontendUrl;
-
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins(frontendUrl)
-                        // .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                        .allowedMethods("GET")
+                        .allowedOrigins("http://localhost:4200")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        // .allowedMethods("GET")
                         .allowCredentials(true);
             }
         };
--- SpringBootAngularOracleXE/backend/src/main/java/br/com/techthor/ecommerce/dao/ProductRepository.java
+++ SpringBootAngularOracleXE/backend/src/main/java/br/com/techthor/ecommerce/dao/ProductRepository.java
@@ -11,7 +11,9 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.rest.core.annotation.RestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RepositoryRestResource
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
