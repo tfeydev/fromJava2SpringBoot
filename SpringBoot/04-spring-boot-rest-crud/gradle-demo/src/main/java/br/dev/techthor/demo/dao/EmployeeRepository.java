3e026d7fc7ed99dab69d4b6d9afa26a523ddd375
tfeydev <thorstenfey@outlook.com>
2025-09-20 17:36:59 -0300

    update sorting, paging, swagger ui

--- SpringBoot/04-spring-boot-rest-crud/gradle-demo/src/main/java/br/dev/techthor/demo/dao/EmployeeRepository.java
+++ SpringBoot/04-spring-boot-rest-crud/gradle-demo/src/main/java/br/dev/techthor/demo/dao/EmployeeRepository.java
@@ -2,6 +2,8 @@ package br.dev.techthor.demo.dao;
 
 import br.dev.techthor.demo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+// @RepositoryRestResource(path = "members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 }
