09062ff39317fd045708bc61dcd98b845b5c2acc
tfeydev <thorstenfey@outlook.com>
2025-09-21 16:54:40 -0300

    update

---
+++ SpringBoot/05-spring-boot-rest-security/src/main/java/br/dev/techthor/demo/security/DemoSecurityConfig.java
@@ -0,0 +1,85 @@
+package br.dev.techthor.demo.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
+
+
+@Configuration
+public class DemoSecurityConfig {
+
+    // add support for JDBC ... no more hardcoded users
+
+    @Bean
+    public UserDetailsManager userDetailsManager(DataSource dataSource) {
+        
+        JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
+
+        // define query to retrieve a user by username
+        jdbcUserDetailsManager.setUsersByUsernameQuery(
+            "SELECT user_id, pw, active FROM employee_directory.members WHERE user_id = ?");
+        
+            // define query to retrieve authorities by username
+        jdbcUserDetailsManager.setAuthoritiesByUsernameQuery(
+            "SELECT user_id, role FROM employee_directory.roles WHERE user_id = ?");
+
+        return jdbcUserDetailsManager;
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        
+        http.authorizeHttpRequests(configurer ->
+            configurer
+                .requestMatchers(HttpMethod.GET, "/api/employees").hasRole("EMPLOYEE")
+                .requestMatchers(HttpMethod.GET, "/api/employees/**").hasRole("EMPLOYEE")
+                .requestMatchers(HttpMethod.POST, "/api/employees").hasRole("MANAGER")
+                .requestMatchers(HttpMethod.PUT, "/api/employees/**").hasRole("MANAGER")
+                .requestMatchers(HttpMethod.PATCH, "/api/employees/**").hasRole("MANAGER")
+                .requestMatchers(HttpMethod.DELETE, "/api/employees/**").hasRole("ADMIN")
+        );
+
+        // use HTTP Basic authentication
+        http.httpBasic(Customizer.withDefaults());
+
+        // in general, not required for stateless REST APIs that use POST, PUT, DELETE and/or PATCH
+        http.csrf(csrf -> csrf.disable());
+
+        return http.build();
+    }
+
+}
+
+
+   /* 
+    @Bean
+    public InMemoryUserDetailsManager userDetailsManager() {
+        
+        UserDetails john = User.builder()
+            .username("john")
+            .password("{noop}test123")
+            .roles("EMPLOYEE")
+            .build();
+
+        UserDetails mary = User.builder()
+            .username("mary")
+            .password("{noop}test123")
+            .roles("EMPLOYEE", "MANAGER")
+            .build();
+
+        UserDetails susan = User.builder()
+            .username("susan")
+            .password("{noop}test123")
+            .roles("EMPLOYEE", "MANAGER", "ADMIN")
+            .build();   
+
+        return new InMemoryUserDetailsManager(john, mary, susan);
+    }
+*/
---
+++ SpringBoot/05-spring-boot-spring-mvc/01-thymeleafdemo-helloworld/src/test/java/br/dev/techthor/thymeleafdemo/ThymeleafdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.dev.techthor.thymeleafdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
