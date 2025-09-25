544e94554f41a51a4dce7ec3a292fd6ff233ec0b
tfeydev <thorstenfey@outlook.com>
2025-09-25 12:34:26 -0300

    update

---
+++ SpringBoot/05-spring-boot-spring-mvc/01-thymeleafdemo-helloworld/src/main/java/br/dev/techthor/thymeleafdemo/controller/HelloWorldController.java
@@ -0,0 +1,62 @@
+package br.dev.techthor.thymeleafdemo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+
+@Controller
+public class HelloWorldController {
+
+    // need a controller method to show the initial HTML form
+    @GetMapping("/showForm")
+    public String showForm() {
+        return "helloworld-form";
+    }
+
+    // need a controller method to process the HTML form
+    @RequestMapping("/processForm")
+    public String processForm() {
+        return "helloworld";
+    }
+
+    // need a controller method to read from data and 
+    // add data to the model
+    @RequestMapping("/processFormVersionTwo")
+    public String letsShoutDude(HttpServletRequest request, Model model) {
+
+        // read the request parameter from the HTML form
+        String theName = request.getParameter("studentName");
+
+        // convert the data to all caps
+        theName = theName.toUpperCase();
+
+        // create the message
+        String result = "Yo! " + theName;
+
+        // add message to the model
+        model.addAttribute("message", result);
+
+        return "helloworld";
+    }
+
+    @GetMapping("/processFormVersionThree")
+    public String processFromVersionThree(@RequestParam("studentName") String theName, Model model) {
+
+        // convert the data to all caps
+        theName = theName.toUpperCase();
+
+        // create the message
+        String result = "Hey My Friend from v3! " + theName;
+
+        // add message to the model
+        model.addAttribute("message", result);
+
+        return "helloworld";
+    }
+
+}
